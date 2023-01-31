54a157a0da8b918c901b4c40c94b2e1286e72277
Soffia <schoolgirl4991@gmail.com>
2023-01-31 19:41:16 +0200

    Create footer, header, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+User.create!(first_name: "Sofiia", last_name: "Shkilna", phone: "380665154604", email: "schoolgirl4991@gmail.com", password: "sofiiaa", password_confirmation: "sofiiaa")
+User.create!(first_name: "Nikita", last_name: "Shkilna", phone: "380665154600", email: "nikita4991@gmail.com", password: "sofiiaa", password_confirmation: "sofiiaa")
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
