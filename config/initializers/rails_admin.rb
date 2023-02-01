f4d29e4cf2a35d79f43dfd62cb29a9e203b39423
Soffia <schoolgirl4991@gmail.com>
2023-02-01 19:55:26 +0200

    Add desine for button and form

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,25 +4,12 @@ RailsAdmin.config do |config|
   config.authorize_with do
     redirect_to main_app.root_path unless current_user.is_admin?
   end
-  
-  ### Popular gems integration
 
   ## == Devise ==
   # config.authenticate_with do
   #   warden.authenticate! scope: :user
   # end
   # config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
 
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
