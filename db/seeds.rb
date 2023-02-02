be05ab07854666daf7e6c7f9cbe5e205f13488ff
Soffia <schoolgirl4991@gmail.com>
2023-02-02 14:03:08 +0200

    Add seed two categories, products for categories 'movie', 'book', I put the link_to instead of the link tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,10 @@ User.create!(first_name: "Nikita",
             password: "nikita", 
             password_confirmation: "nikita",
             is_admin: true)
-
+# Create categories
 Category.create!(name: "Game", position: 1)
-
-# Product.create!(name: "Demo", price: 100, description: "description", position: 1, category: Category.find_by(name: "Game"))
+Category.create!(name: "Movie", position: 2)
+Category.create!(name: "Book", position: 3)
 
 10.times do |n|
     # а треба тут картинку генерити?
@@ -30,3 +30,28 @@ Category.create!(name: "Game", position: 1)
     category = Category.find_by(name: "Game")
     Product.create!(name: name, price: price, description: description, position: position, category: category)
 end
+
+10.times do |n|
+    # а треба тут картинку генерити?
+    # Faker::LoremFlickr.image(size: "100x100", search_terms: ['games'])
+    name = Faker::Movie.title
+    price = Faker::Commerce.price(range: 0..100)
+    description = Faker::Movie.quote
+    position = "#{n+1}"
+    # це треба поміняти? на category_id
+    category = Category.find_by(name: "Movie")
+    Product.create!(name: name, price: price, description: description, position: position, category: category)
+end
+
+10.times do |n|
+    # а треба тут картинку генерити?
+    # Faker::LoremFlickr.image(size: "100x100", search_terms: ['games'])
+    name = Faker::Book.title
+    price = Faker::Commerce.price(range: 0..100)
+    description = Faker::Book.genre
+    position = "#{n+1}"
+    # це треба поміняти? на category_id
+    category = Category.find_by(name: "Book")
+    Product.create!(name: name, price: price, description: description, position: position, category: category)
+end
+
